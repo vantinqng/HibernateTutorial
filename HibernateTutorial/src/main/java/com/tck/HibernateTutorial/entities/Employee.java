4a4549dc963c4d2347ee73eb60b32cbd19f7143b
Tin TCK <vantinqng@gmail.com>
2019-10-21 19:08:48 +0700

    Hibernate tutorial

---
+++ HibernateTutorial/src/main/java/com/tck/HibernateTutorial/entities/Employee.java
@@ -0,0 +1,123 @@
+package com.tck.HibernateTutorial.entities;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name="EMPLOYEE",
+uniqueConstraints = {@UniqueConstraint(columnNames = {"EMP_NO"})})
+public class Employee {
+	private Long empId;
+	private String empNo;
+	private String empName;
+	private String job;
+	private Employee manager;
+	private Date hireDate;
+	private Float salary;
+	private byte[] image;
+	
+	private Department department;
+	private Set<Employee> employees=new HashSet<Employee>(0);
+	public Employee() {
+	}
+	public Employee(Long empId, String empName, String job, Employee manager, Date hireDate, Float salary,
+			Department department) {
+		this.empId = empId;
+		this.empNo = "E"+ this.empId;
+		this.empName = empName;
+		this.job = job;
+		this.manager = manager;
+		this.hireDate = hireDate;
+		this.salary = salary;
+		this.department = department;
+	}
+	@Id
+	@Column(name="EMP_ID")
+	public Long getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Long empId) {
+		this.empId = empId;
+	}
+	@Column(name="EMP_NO", length=20,nullable = false)
+	public String getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(String empNo) {
+		this.empNo = empNo;
+	}
+	@Column(name="EMP_NAME", length = 50, nullable = false)
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	@Column(name="JOB", length = 50, nullable = false)
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="MNG_ID")
+	public Employee getManager() {
+		return manager;
+	}
+	public void setManager(Employee manager) {
+		this.manager = manager;
+	}
+	@Column(name="HIRE_DATE", nullable = false)
+	public Date getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+	@Column(name="SALARY", nullable = false)
+	public Float getSalary() {
+		return salary;
+	}
+	public void setSalary(Float salary) {
+		this.salary = salary;
+	}
+	@Column(name="IMAGE", length = 1111111, nullable = true)
+	public byte[] getImage() {
+		return image;
+	}
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "DEPT_ID", nullable = false)
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = empId)
+	public Set<Employee> getEmployees() {
+		return employees;
+	}
+	public void setEmployees(Set<Employee> employees) {
+		this.employees = employees;
+	}
+	
+	
+	
+	
+
+}
